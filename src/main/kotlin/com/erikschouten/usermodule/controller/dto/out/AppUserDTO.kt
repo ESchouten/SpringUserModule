e3e572bf1c8eaa96b513debe75a9d20cc38c09b8
Erik Schouten <Erik@ErikSchouten.com>
2018-08-03 16:00:32 +0200

    Extendable AppUserDTO

--- src/main/kotlin/com/erikschouten/usermodule/controller/dto/out/AppUserDTO.kt
+++ src/main/kotlin/com/erikschouten/usermodule/controller/dto/out/AppUserDTO.kt
@@ -4,7 +4,7 @@ import com.erikschouten.usermodule.model.AppUser
 import java.util.*
 import javax.validation.constraints.Email
 
-data class AppUserDTO(val id: UUID,
+open class AppUserDTO(val id: UUID,
                       @field:[Email] val email: String,
                       val authorities: List<String>,
                       val locked: Boolean) {
