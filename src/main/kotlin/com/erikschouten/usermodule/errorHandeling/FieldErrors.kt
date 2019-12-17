838084e2d367e71a33a54851c0ff406631ad7536
Calvin Bijsmans <calvinbijsmans@hotmail.com>
2019-12-17 09:27:25 +0100

    UNPROCESSABLE_ENTITY

--- src/main/kotlin/com/erikschouten/usermodule/errorHandeling/FieldErrors.kt
+++ src/main/kotlin/com/erikschouten/usermodule/errorHandeling/FieldErrors.kt
@@ -3,5 +3,5 @@ package com.erikschouten.usermodule.errorHandeling
 import org.springframework.http.HttpStatus
 
 enum class FieldErrors(val status: HttpStatus = HttpStatus.BAD_REQUEST) {
-    NOT_FOUND(HttpStatus.NOT_FOUND), ALREADY_EXISTS(HttpStatus.CONFLICT), CONFLICT, EXPECTATION_FAILED(HttpStatus.EXPECTATION_FAILED)
+    NOT_FOUND(HttpStatus.NOT_FOUND), ALREADY_EXISTS(HttpStatus.CONFLICT), CONFLICT, EXPECTATION_FAILED(HttpStatus.EXPECTATION_FAILED), UNPROCESSABLE_ENTITY(HttpStatus.UNPROCESSABLE_ENTITY)
 }
