156a911e5bbcdd523dd59b8315a2ce2cd9a25c6b
Erik Schouten <Erik@ErikSchouten.com>
2020-09-21 14:13:02 +0200

    FieldErrorExceptionDTO added

--- src/main/kotlin/com/erikschouten/usermodule/errorhandeling/FieldErrorException.kt
+++ src/main/kotlin/com/erikschouten/usermodule/errorhandeling/FieldErrorException.kt
@@ -5,4 +5,13 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 
 fun FieldErrorException.toResponse() =
-    ResponseEntity(this, status?.let { HttpStatus.valueOf(it) } ?: HttpStatus.BAD_REQUEST)
+    ResponseEntity(FieldErrorExceptionDTO(this), status?.let { HttpStatus.valueOf(it) } ?: HttpStatus.BAD_REQUEST)
+
+class FieldErrorExceptionDTO private constructor(
+    val objectName: String? = null,
+    val field: String? = null,
+    val status: Int? = null,
+    val description: String? = null
+) {
+    constructor(exception: FieldErrorException) : this(exception.objectName, exception.field, exception.status, exception.description)
+}
